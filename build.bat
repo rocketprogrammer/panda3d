b97680cd67fed67f69e5f557974c6d0b7802d721
Rocket Programmer <rocketgamer580@gmail.com>
2019-11-24 19:19:07 +0000

    build: Use py2

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 title - Panda3D Builder
 color 4
 :main
-makepanda\makepanda.bat --everything --installer --msvc-version=14.2 --no-eigen --windows-sdk 10 --threads=4
+"thirdparty/win-python-x64/python.exe" makepanda/makepanda.py --everything --installer --msvc-version=14.2 --no-eigen --windows-sdk 10 --threads=4
 echo Done!
 pause
 goto :main
