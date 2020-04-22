95dd47a46a0390b66be770338778e5718d204399
EliasTDev <38441309+EliasTDev@users.noreply.github.com>
2020-04-22 10:22:28 -0400

    fixes

--- direct/src/distributed/DistributedObjectAI.py
+++ direct/src/distributed/DistributedObjectAI.py
@@ -349,16 +349,16 @@ class DistributedObjectAI(DistributedObjectBase):
             self.air.sendUpdate(self, fieldName, args)
 
     def GetPuppetConnectionChannel(self, doId):
-        return doId + (1L << 32)
+        return doId + (1001 << 32)
 
     def GetAccountConnectionChannel(self, doId):
-        return doId + (3L << 32)
+        return doId + (1003 << 32)
 
     def GetAccountIDFromChannelCode(self, channel):
         return channel >> 32
 
     def GetAvatarIDFromChannelCode(self, channel):
-        return channel & 0xffffffffL
+        return channel & 0xffffffff
 
     def sendUpdateToAvatarId(self, avId, fieldName, args):
         assert self.notify.debugStateCall(self)
