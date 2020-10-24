5d2cc765c3707fadfd69b17e69af3b096478e7f0
Rocket Programmer <rocketgamer580@gmail.com>
2020-10-23 20:57:03 -0700

    build: Remove colored console

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
 title - Panda3D Builder
-color 4
 :main
 "thirdparty/win-python-x64/python.exe" makepanda/makepanda.py --everything --installer --msvc-version=14.2 --no-eigen --windows-sdk 10 --threads=2
 echo Done!
