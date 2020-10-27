403bd869348d4736cba9df9645e676812df2488d
Rocket Programmer <rocketgamer580@gmail.com>
2020-10-27 16:14:05 -0700

    these

--- makepanda/makepanda.py
+++ makepanda/makepanda.py
@@ -2590,14 +2590,6 @@ for basename in del_files:
 p3d_init = """"Python bindings for the Panda3D libraries"
 
 __version__ = '%s'
-
-if __debug__:
-    import sys
-    if sys.version_info < (3, 0):
-        sys.stderr.write("WARNING: Python 2.7 has reached EOL as of January 1, 2020.\\n")
-        sys.stderr.write("To suppress this warning, upgrade to Python 3.\\n")
-        sys.stderr.flush()
-    del sys
 """ % (WHLVERSION)
 
 if GetTarget() == 'windows':
