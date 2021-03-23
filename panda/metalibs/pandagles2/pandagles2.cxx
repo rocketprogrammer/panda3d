28793e35e2f675f2bd4a48439a45de25b55f2961
Rocket Programmer <rocketgamer580@gmail.com>
2021-03-22 22:05:30 -0700

    Update pandagles2.cxx

--- panda/metalibs/pandagles2/pandagles2.cxx
+++ panda/metalibs/pandagles2/pandagles2.cxx
@@ -1,70 +1,48 @@
 /**
- * @file pandagles2.cxx
+ * @file pandagles.cxx
  * @author rdb
  * @date 2009-06-08
  */
-#ifndef __ANDROID__
-#include "pandagles2.h"
 
-#define OPENGLES_2
-#include "config_gles2gsg.h"
+#include "pandagles.h"
 
+#define OPENGLES_1
+#include "config_glesgsg.h"
+
+#if defined(ANDROID)
+#include "config_androiddisplay.h"
+#include "androidGraphicsPipe.h"
+#else
 #include "config_egldisplay.h"
 #include "eglGraphicsPipe.h"
-
-/**
- * Initializes the library.  This must be called at least once before any of
- * the functions or classes in this library can be used.  Normally it will be
- * called by the static initializers and need not be called explicitly, but
- * special cases exist.
- */
-void
-init_libpandagles2() {
-  init_libgles2gsg();
-  init_libegldisplay();
-}
-
-/**
- * Returns the TypeHandle index of the recommended graphics pipe type defined
- * by this module.
- */
-int
-get_pipe_type_pandagles2() {
-  return eglGraphicsPipe::get_class_type().get_index();
-}
-
-#else
-
-#include "pandagles2.h"
-
-#define OPENGLES_2
-#include "config_gles2gsg.h"
-
-//#include "config_egldisplay.h"
-//#include "eglGraphicsPipe.h"
-#include "config_androiddisplay.h"
-#include "viewGraphicsPipe.h"
-
-/**
- * Initializes the library.  This must be called at least once before any of
- * the functions or classes in this library can be used.  Normally it will be
- * called by the static initializers and need not be called explicitly, but
- * special cases exist.
- */
-void
-init_libpandagles2() {
-  init_libgles2gsg();
-  init_libviewdisplay();
-}
-
-/**
- * Returns the TypeHandle index of the recommended graphics pipe type defined
- * by this module.
- */
-int
-get_pipe_type_pandagles2() {
-  return viewGraphicsPipe::get_class_type().get_index();
-}
-
-
 #endif
+
+/**
+ * Initializes the library.  This must be called at least once before any of
+ * the functions or classes in this library can be used.  Normally it will be
+ * called by the static initializers and need not be called explicitly, but
+ * special cases exist.
+ */
+void
+init_libpandagles() {
+  init_libglesgsg();
+
+#if defined(ANDROID)
+  init_libandroiddisplay();
+#else
+  init_libegldisplay();
+#endif
+}
+
+/**
+ * Returns the TypeHandle index of the recommended graphics pipe type defined
+ * by this module.
+ */
+int
+get_pipe_type_pandagles() {
+#if defined(ANDROID)
+  return AndroidGraphicsPipe::get_class_type().get_index();
+#else
+  return eglGraphicsPipe::get_class_type().get_index();
+#endif
+}
