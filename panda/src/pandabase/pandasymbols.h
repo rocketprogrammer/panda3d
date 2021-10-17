abce7c284b3522295e575c7b4f94e3dfe7a6c78f
Rocket Programmer <rocketgamer580@gmail.com>
2021-10-16 23:40:57 -0700

    pandabase: Remove DNA

--- panda/src/pandabase/pandasymbols.h
+++ panda/src/pandabase/pandasymbols.h
@@ -587,14 +587,6 @@
   #define EXPTP_VRPN IMPORT_TEMPL
 #endif
 
-#ifdef BUILDING_DNA
-    #define EXPCL_DNA EXPORT_CLASS
-    #define EXPTP_DNA EXPORT_TEMPL
-#else
-   #define EXPCL_DNA IMPORT_CLASS
-   #define EXPTP_DNA IMPORT_TEMPL
-#endif
-
 #if defined(_MSC_VER) && !defined(CPPPARSER)
 #define INLINE_LINMATH __forceinline
 #define INLINE_MATHUTIL __forceinline
