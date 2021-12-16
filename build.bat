e98f1ede13cf672f679edf74b38340c0286c3aa0
Rocket Programmer <rocketgamer580@gmail.com>
2021-12-16 04:12:29 -0800

    build: Update

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 title - Panda3D Builder
 color 4
 :main
-"thirdparty/win-python3.9-x64/python.exe" makepanda\makepanda.py --everything --installer --msvc-version=14.2 --no-eigen --windows-sdk 10 --threads=16
+"thirdparty/win-python3.9-x64/python.exe" makepanda\makepanda.py --everything --installer --msvc-version=14.3 --no-eigen --threads=16
 echo Done!
 pause
 goto :main
