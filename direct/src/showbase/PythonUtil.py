ecdd45f7c10842c9ee86ca10269a454f56b95442
Rocket <me@rocketprogrammer.me>
2022-08-14 19:17:13 -0500

    python-util: Fix imports

--- direct/src/showbase/PythonUtil.py
+++ direct/src/showbase/PythonUtil.py
@@ -50,6 +50,7 @@ __report_indent = 3
 from panda3d.core import ConfigVariableBool, ClockObject
 from html.parser import HTMLParser
 import xml.etree.ElementTree as ET
+import unicodedata
 
 ## with one integer positional arg, this uses about 4/5 of the memory of the Functor class below
 #def Functor(function, *args, **kArgs):
