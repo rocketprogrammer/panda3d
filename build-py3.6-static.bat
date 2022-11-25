9a2e3f0059e3def54376e49c404afd7492208cc7
Rocket <me@rocketprogrammer.me>
2022-11-25 16:41:21 -0600

    builder: Update

--- build-py3.6-static.bat
+++ build-py3.6-static.bat
@@ -2,7 +2,7 @@
 title - Panda3D Builder
 color 4
 :main
-"thirdparty/win-python3.6-x64/python.exe" makepanda/makepanda.py --everything --installer --msvc-version=14.2 --no-eigen --no-vision --no-vrpn --no-skel --windows-sdk 10 --threads=16 --static
+"thirdparty/win-python3.6-x64/python.exe" makepanda/makepanda.py --everything --installer --msvc-version=14.3 --no-eigen --no-vision --no-vrpn --no-skel --windows-sdk 10 --threads=16 --static
 echo Done!
 pause
 goto :main
