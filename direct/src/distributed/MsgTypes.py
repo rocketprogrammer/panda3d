37ecf864df5c686e2ed413272975d586466dc54c
Rocket <me@rocketprogrammer.me>
2022-12-03 15:27:57 -0600

    distributed: Add this back

--- direct/src/distributed/MsgTypes.py
+++ direct/src/distributed/MsgTypes.py
@@ -1,6 +1,7 @@
 """MsgTypes module: contains distributed object message types"""
 
 from direct.showbase.PythonUtil import invertDictLossless
+from .DisneyMessageTypes import *
 
 MsgName2Id = {
     'CLIENT_HELLO':                                  1,
