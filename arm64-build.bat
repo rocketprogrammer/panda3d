d66bf997ed6a10049aef081b9ed2f29add8c6f87
Rocket <me@rocketprogrammer.me>
2024-06-08 15:24:31 -0500

    repo: Push to move to laptop

---
+++ arm64-build.bat
@@ -0,0 +1,11 @@
+@echo off
+title - Panda3D Builder
+color 4
+
+set MAKEPANDA_THIRDPARTY=thirdparty_arm64
+
+:main
+"thirdparty/win-python3.11-x64/python.exe" makepanda\makepanda.py --everything --installer --msvc-version=14.3 --no-eigen --windows-sdk 10 --threads=16 --arch arm64
+echo Done!
+pause
+goto :main
