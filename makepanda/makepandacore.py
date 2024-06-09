2a5c4c8f9c8781cacf77b26be7a55ebaf3c19fab
Rocket <me@rocketprogrammer.me>
2024-06-09 00:03:39 -0500

    makepanda: Flip conditional

--- makepanda/makepandacore.py
+++ makepanda/makepandacore.py
@@ -2749,7 +2749,7 @@ def SetupVisualStudioEnviron():
     bindir = ""
     libdir = ""
     if ("VCTOOLSVERSION" in SDK):
-        bindir = "Host" + GetHostArch().upper() if arch != "arm64" else "Hostarm64" + "\\" + arch
+        bindir = "Host" + "Hostarm64" if arch == "arm64" else GetHostArch().upper() + "\\" + arch
         libdir = arch
     else:
         if (arch == 'x64'):
