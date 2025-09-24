2e7da892bee09667b83faf9bd968b3a6bb1720cf
Jaden <me@rocketprogrammer.me>
2025-09-23 21:15:09 -0500

    Revert "Update build.bat"
    This reverts commit 3a17e2fcdca2e52f69b1ce43e2d2ba876d6dd067.

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 title - Panda3D Builder
 color 4
 :main
-"thirdparty/win-python3.12-x64/python.exe" makepanda\makepanda.py --everything --installer --msvc-version=14.3 --no-eigen --windows-sdk 10 --threads=8 --outputdir built_Pirates
+"thirdparty/win-python3.13-x64/python.exe" makepanda\makepanda.py --everything --installer --msvc-version=14.3 --no-eigen --windows-sdk 10 --threads=8 --outputdir built_Pirates
 echo Done!
 pause
 goto :main
