63c759c152226167030ab8e67546e590bb12db10
Jaden <me@rocketprogrammer.me>
2026-07-30 00:52:28 -0500

    opt4 build

--- x86-build.bat
+++ x86-build.bat
@@ -2,7 +2,7 @@
 title - Panda3D Builder
 color 4
 :main
-"thirdparty_x86/win-python3.12/python.exe" makepanda\makepanda.py --everything --installer --msvc-version=14.3 --no-eigen --windows-sdk 10 --threads=16 --no-maya2022
+"thirdparty_x86/win-python3.12/python.exe" makepanda\makepanda.py --optimize 4 --everything --installer --msvc-version=14.3 --no-eigen --windows-sdk 10 --threads=16 --no-maya2022
 echo Done!
 pause
 goto :main
